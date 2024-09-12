647eb20a6f4558fdbf59f31bbe1ea4413e623453
Farida770 <farida.m.faheem@gmail.com>
2024-09-11 21:56:54 -0500

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+    public static int area(int l, int w) 
+    {
+        int area = l*w;
+        return area;
+    }
+    public static int perimeter(int l, int w)
+    {
+        int perimeter = l+l+w+w;
+        return perimeter;
+    }
+}
---
+++ Sales.java
@@ -0,0 +1,11 @@
+public class Sales 
+{
+    
+    public static double totalCost(double cost)  
+    {
+        return cost * 1.0825;
+    }  
+
+}
+
+    
